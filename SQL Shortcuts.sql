00ab11f7d0ab6434ae88e82caec85969d9c3281c
DummyGod <40311525+DummyGod@users.noreply.github.com>
2020-06-09 09:42:23 +0530

    Add files via upload

---
+++ SQL Shortcuts.sql
@@ -0,0 +1,7 @@
+select * from INFORMATION_SCHEMA.COLUMNS where COLUMN_NAME like 
+
+sp_helptext 
+
+select * from INFORMATION_SCHEMA.TABLES where TABLE_NAME like 
+
+SELECT DISTINCT so.name FROM syscomments sc INNER JOIN sysobjects so ON sc.id=so.id WHERE sc.TEXT LIKE 
